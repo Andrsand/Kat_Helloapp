20d988c7d02d67c8acd29a49c15234224bd8afd2
andrsand <angorko@list.ru>
2024-03-10 17:40:41 +0300

    remainder of the division

--- src/Program.java
+++ src/Program.java
@@ -1,17 +1,12 @@
 
 
 public class Program {
-    public static void main(String[] args) {
-        System.out.println(fillArray(4));
+    public static void main(String[] args){
+        System.out.println(drawisMonitorsCounter(17, 8));
     }
-
-    static int[] fillArray(int x) {
-        int[] array = new int[x];
-        for (int i = 0; i < x; i++) {
-            array[i] = i;
+    public static int drawisMonitorsCounter(int monitors, int programmers) {
+        return monitors % programmers;
         }
-        return array;
-    }
 }
 
 
