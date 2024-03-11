546f62f65224bcae3212850f8e474a73e0fa178c
andrsand <angorko@list.ru>
2024-03-11 14:15:58 +0300

    Converting int to char - 1

--- src/Program.java
+++ src/Program.java
@@ -1,16 +1,11 @@
-import java.math.BigInteger;
-
-import static java.lang.Math.sqrt;
-
 public class Program {
     public static void main(String[] args) {
         charExpression(1);
         }
-    public static char charExpression(int a) {
+    public static void charExpression(int a) {
         int sl = (int) '\\' + a;
         char symbol = (char) sl;
         System.out.println(symbol);
-        return symbol;
     }
 }
 
