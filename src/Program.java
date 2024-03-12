72ef846c206aa05cbdf1d2f33beee5bcce96c467
andrsand <angorko@list.ru>
2024-03-12 13:42:27 +0300

    Factorial calculation with recursion

--- src/Program.java
+++ src/Program.java
@@ -4,12 +4,13 @@ public class Program {
     public static void main(String[] args) {
         System.out.println(factorial(5));
     }
+    //
     public static BigInteger factorial(int value) {
-        BigInteger result = BigInteger.ONE;
-        for (int i = 1; i <= value; i++) {
-            result = result.multiply(BigInteger.valueOf(i));
+        if (value <= 1) {
+            return BigInteger.valueOf(1);
+        } else {
+            return BigInteger.valueOf(value).multiply(factorial(value - 1));
         }
-        return result;
     }
 }
 
