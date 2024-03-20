34d6be05f90f7a55c9e7329e5d094c4b91937de1
andrsand <angorko@list.ru>
2024-03-20 20:11:55 +0300

    Array inversion

--- src/Program.java
+++ src/Program.java
@@ -6,25 +6,23 @@ import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        int[] numbers = {0};
-        int[] middle = getArrayMiddle(numbers);
-        for (int num : middle) {
+        int[] arr = {8, 4, 7, 8, 9, 0};
+        arr = inverseArray(arr);
+        for (int num : arr) {
             System.out.print(num + " ");
         }
 
     }
-
-    public static int[] getArrayMiddle(int[] numbers) {
-        int length = numbers.length;
-        if(numbers.length == 0){
-            return new int[0];
+    public static int[] inverseArray(int[] numbers) {
+        int[] result = new int[numbers.length]; // присваиваем массиву равнозначную numbers длину
+        int index = 0;
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            result[index] = numbers[i];
+            index++;
         }
-        if (length % 2 == 0) {
-            return new int[]{numbers[length / 2 - 1], numbers[length / 2]}; // 2-1 - вывод индекса - 1 а дальше по аналогии...
-        } else {
-            return new int[]{numbers[length / 2]};
-        }
+        return result;
     }
+
 }
 
 
