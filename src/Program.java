ff1f66f6ca2b9687bcc554c8a24c0190f3deacda
andrsand <angorko@list.ru>
2024-03-21 17:49:12 +0300

    Bubble sorting an array and merging it into one array

--- src/Program.java
+++ src/Program.java
@@ -1,13 +1,7 @@
-import java.io.OptionalDataException;
-import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Program {
     public static void main(String[] args) {
-        int[] firstArray = {};
-        int[] secondArray = {1, 4, 3};
+        int[] firstArray = {1, 3, 7, 5};
+        int[] secondArray = {8, 4, 2, 4};
 
         int[] mergedAndSortedArray = mergeAndSort(firstArray, secondArray);
 
@@ -16,73 +10,63 @@ public class Program {
         }
     }
 
+    //метод - объединяет два массива и потом сортирует их
     public static int[] mergeAndSort(int[] firstArray, int[] secondArray) {
-        if (firstArray.length == 0 && secondArray.length == 0) {
+        if (firstArray.length == 0 && secondArray.length == 0) { // сли первый массив и второй массив пустые, то вернет пустой массив
             return new int[]{};
-        } else if (firstArray.length == 0) {
-            return sortArray(secondArray);
-        } else if (secondArray.length == 0) {
+        } else if (firstArray.length == 0) {  // если первый массив пустой, то сортирует и возвращает второй массив
+            return sortArray(secondArray);   // sortArray - используем отдельный метод пузырьковой сортировки
+        } else if (secondArray.length == 0) {  //если второй массив пустой, то сортирует и возвращает первый массив
             return sortArray(firstArray);
         } else {
-            int[] mergedArray = new int[firstArray.length + secondArray.length];
-            int i = 0, j = 0, k = 0;
+            int[] finalArray = new int[firstArray.length + secondArray.length]; //иначе создает новый массив для объединения двух массивов
+            int i = 0, j = 0, k = 0; //создает указатели для первого, второго и объединенного массива
 
-            while (i < firstArray.length && j < secondArray.length) {
-                if (firstArray[i] < secondArray[j]) {
-                    mergedArray[k++] = firstArray[i++];
+            // циклы - на случай если из массивов - пустой. Этот блок кода в принципе - лишний
+            while (i < firstArray.length && j < secondArray.length) { //  Wикл while - пока i меньше длины первого массива и j меньше длины второго массива
+                if (firstArray[i] < secondArray[j]) {   // Если значение из первого массива меньше значения из второго массива, добавляем его в объединенный массив и увеличиваем i на 1
+                    finalArray[k++] = firstArray[i++];
                 } else {
-                    mergedArray[k++] = secondArray[j++];
+                    finalArray[k++] = secondArray[j++];  // Иначе, добавляем значение из второго массива в объединенный массив и увеличиваем j на 1
                 }
             }
 
-            while (i < firstArray.length) {
-                mergedArray[k++] = firstArray[i++];
+            while (i < firstArray.length) {         // Добавляем оставшиеся элементы из первого массива в объединенный массив
+                finalArray[k++] = firstArray[i++];
             }
-            while (j < secondArray.length) {
-                mergedArray[k++] = secondArray[j++];
+            while (j < secondArray.length) {        // И оставшиеся элементы из второго массива
+                finalArray[k++] = secondArray[j++];
             }
 
-            return sortArray(mergedArray);
+            return sortArray(finalArray);           // Возвращаем объединенный массив, отсортированный методом sortArray
         }
     }
 
-    //сортировка массива методом пузырька
-    private static int[] sortArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+    // метод сортировки массива способом пузырька
+    // перебирает элементы массива и меняет их местами, если текущий элемент больше следующего
+    public static int[] sortArray(int[] array) {
+        for (int i = 0; i < array.length; i++) { // После завершения внешнего цикла весь массив будет отсортирован
+            for (int j = i + 1; j < array.length; j++) { //  После завершения внутреннего цикла все элементы до i-того будут отсортированы
+                if (array[i] > array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                 }
             }
         }
-        return arr;
+        return array; // Функция возвращает отсортированный массив
     }
 }
 
-//этот метод объединяет два массива и потом сортирует их
-//если первый массив и второй массив пустые, то вернет пустой массив
-//если первый массив пустой, то сортирует и возвращает второй массив
-//если второй массив пустой, то сортирует и возвращает первый массив
-//иначе создает новый массив для объединения двух массивов
-//создает указатели для первого, второго и объединенного массива
-
-// Объявляем цикл while, который будет выполняться, пока i меньше длины первого массива и j меньше длины второго массива
-// Если значение из первого массива меньше значения из второго массива, добавляем его в объединенный массив и увеличиваем i на 1
-// Иначе, добавляем значение из второго массива в объединенный массив и увеличиваем j на 1
-
-// Далее добавляем оставшиеся элементы из первого массива в объединенный массив
-// И оставшиеся элементы из второго массива
-
-// Возвращаем объединенный массив, отсортированный функцией sortArray
 
 
-// Этот код представляет собой сортировку массива методом пузырька
-// Он перебирает элементы массива и меняет их местами, если текущий элемент больше следующего
-// После завершения внутреннего цикла все элементы до i-того будут отсортированы
-// После завершения внешнего цикла весь массив будет отсортирован
-// Функция возвращает отсортированный массив
+
+
+
+
+
+
+
 
 
 
